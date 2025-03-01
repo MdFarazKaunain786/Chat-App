b1f5262b9f31820a1063f5622f7d5fe0b2f17162
Farazkaunain18 <mdfarazkaunain786@gmail.com>
2025-03-02 02:37:53 +0530

    final

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'MindScan'
+  title: 'Mental Health'
 }
 
 export default function RootLayout({
